ba82c29564395764b96674aa6226c3f48677d962
Marwen Mahmoudi <marouenmahmoudy@gmailcom>
2021-02-09 20:58:06 +0100

    Final script to extract details of all books and create a csv file

--- OneBook.py
+++ OneBook.py
@@ -2,36 +2,14 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-html = requests.get("https://books.toscrape.com/catalogue/category/books/mystery_3/page-1.html")
-text = BeautifulSoup(html.content, 'html.parser')
+Home = requests.get("https://books.toscrape.com/index.html")
+First = BeautifulSoup(Home.content, 'html.parser')
 
-# pagination
-# get number of pages per category
-page_indicator = text.find(class_='current').get_text().split()
-num_pages = int(page_indicator[3])
-# print(num_pages)
-
-Category_pages_links = []
-current_html = "https://books.toscrape.com/catalogue/category/books/mystery_3/"
-
-for i in range(1, num_pages+1):
-    page_link = current_html+"page-"+str(i)+".html"
-    Category_pages_links.append(page_link)
-# print(Category_pages_list)
 
 Category_books_links = []
-for x in Category_pages_links:
-    html = requests.get(x)
-    text = BeautifulSoup(html.content, 'html.parser')
-    # get books links
-    Container = text.find_all(class_='image_container')
-    for link in Container:
-        href_att = link.a['href']
-        href_txt = href_att.split("../../..")
-        booksLink = "https://books.toscrape.com/catalogue" + href_txt[1]
-        Category_books_links.append(booksLink)
-# print(Category_books_links)
-
+Category_pages_links = []
+# create a list of all categories links
+Categories_list = []
 # Lists of contents to use later in a dictionary
 Titles = []
 Product_page_url = []
@@ -43,48 +21,84 @@ PIncTax = []
 PExTax = []
 ratings = []
 Num_available = []
-for current_link in Category_books_links:
-    r = requests.get(current_link)
-    soup = BeautifulSoup(r.content, 'html.parser')
 
-    Product_page = r.url
-    Product_page_url.append(Product_page)
+ul_tag = First.find(class_='nav nav-list').find_all('a')
+for hrf in ul_tag[1:51]:
+    hrf_att = hrf['href']
+    CategoryLink = "https://books.toscrape.com/"+hrf_att
+    Categories_list.append(CategoryLink)
+#print(Categories_list)
 
-    Title = soup.find('h1').get_text()
-    Titles.append(Title)
+for lin in Categories_list:
+    html = requests.get(lin)
+    wrt = BeautifulSoup(html.content, 'html.parser')
+    # pagination
+    # get number of pages per category
+    Test = wrt.find(class_='current')
+    if Test == None:
+       num_pages = 1
+    else:
+        page_indicator = wrt.find(class_='current').get_text().split()
+        num_pages = int(page_indicator[3])
+    print(num_pages)
+    html_split = lin.split("index")
+    current_html = html_split[0]
+    for i in range(1, num_pages + 1):
+        page_link = current_html + "page-" + str(i) + ".html"
+        Category_pages_links.append(page_link)
+    # print(Category_pages_list)
+    for x in Category_pages_links:
+        html = requests.get(x)
+        text = BeautifulSoup(html.content, 'html.parser')
+        # get books links
+        Container = text.find_all(class_='image_container')
+        for link in Container:
+            href_att = link.a['href']
+            href_txt = href_att.split("../../..")
+            booksLink = "https://books.toscrape.com/catalogue" + href_txt[1]
+            Category_books_links.append(booksLink)
+    # Here we got all the links of all the books of one category
+    # Now we go to extract the details of books
+    for current_link in Category_books_links:
+        r = requests.get(current_link)
+        soup = BeautifulSoup(r.content, 'html.parser')
 
+        Product_page = r.url
+        Product_page_url.append(Product_page)
 
-    Category_a = soup.find(class_="breadcrumb").find_all('a')
-    Category = Category_a[2].string
-    Categories.append(Category)
+        Title = soup.find('h1').get_text()
+        Titles.append(Title)
 
+        Category_a = soup.find(class_="breadcrumb").find_all('a')
+        Category = Category_a[2].string
+        Categories.append(Category)
 
-    Product_Description = soup.find(class_="sub-header").findNext('p').get_text()
-    Product_DescriptionS.append(Product_Description)
+        Product_Description = soup.find(class_="sub-header").findNext('p').get_text()
+        Product_DescriptionS.append(Product_Description)
 
-    src_txt = soup.find(class_='item active').img.get('src')
-    jpg = src_txt.split("../../")
-    Image_url = ('https://books.toscrape.com/' + jpg[1])
-    Image_urlS.append(Image_url)
+        src_txt = soup.find(class_='item active').img.get('src')
+        jpg = src_txt.split("../../")
+        Image_url = ('https://books.toscrape.com/' + jpg[1])
+        Image_urlS.append(Image_url)
 
-    Product_information = soup.find(class_='table table-striped').find_all('td')
+        Product_information = soup.find(class_='table table-striped').find_all('td')
 
-    universal_product_code = Product_information[0].get_text()
-    upc.append(universal_product_code)
+        universal_product_code = Product_information[0].get_text()
+        upc.append(universal_product_code)
 
-    price_excluding_tax = Product_information[2].get_text()
-    PExTax.append(price_excluding_tax)
+        price_excluding_tax = Product_information[2].get_text()
+        PExTax.append(price_excluding_tax)
 
-    price_including_tax = Product_information[3].get_text()
-    PIncTax.append(price_including_tax)
+        price_including_tax = Product_information[3].get_text()
+        PIncTax.append(price_including_tax)
 
-    number_available = Product_information[5].get_text()
-    Num_available.append(number_available)
+        number_available = Product_information[5].get_text()
+        Num_available.append(number_available)
 
-    p_rating = soup.find(class_='col-sm-6 product_main').find_all('p')
-    star = p_rating[2]['class']
-    review_rating = star[1]
-    ratings.append(review_rating)
+        p_rating = soup.find(class_='col-sm-6 product_main').find_all('p')
+        star = p_rating[2]['class']
+        review_rating = star[1]
+        ratings.append(review_rating)
 
 # Dictionary containing all the details
 d = {
@@ -101,6 +115,6 @@ d = {
     }
 
 # Using the dictionary for the DataFrame and creating a csv file
-Book_details = pd.DataFrame(d, index=[1])
-print(Book_details)
-Book_details.to_csv('booksScraping.csv')
+Book_details = pd.DataFrame(data=d)
+# print(Book_details)
+Book_details.to_csv('booksScrapingFinal.csv', index=None)
